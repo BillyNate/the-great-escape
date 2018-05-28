ab732b926c7606979cc368a1c329f4547085dba5
BillyNate <billynate@billynate.com>
2018-05-28 13:41:11 +0200

    Replaced nosleep for a different script

---
+++ pages/sleep-config.js
@@ -0,0 +1,12 @@
+$(function()
+{
+  sleep.prevent();
+  // For some stupid reason the touchstart does not fire the first time...
+  $(document).on('touchstart mousedown', function()
+  {
+    if(sleep._video.paused)
+    {
+      sleep._video.play();
+    }
+  });
+});
